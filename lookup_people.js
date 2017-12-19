afa3da7f7babf757d8d73c8f0f3ba1c64b92cd5e
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-19 20:16:38 +0000

    fixed date format

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
 const settings = require('./settings');
+const moment = require('moment');
 
 const client = new pg.Client({
   user: settings.user,
@@ -16,14 +17,14 @@ client.connect((err) => {
   if (err) {
     return console.error('connection error', err);
   }
-  // console.log(req_param);
-  client.query('SELECT first_name, last_name, birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text', [req_param], (err, result) => {
+
+  client.query('SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text', [req_param], (err, result) => {
     if (err) {
       return console.error('error running query', err);
     }
     console.log('Searching...');
     console.log(`Found ${result.rows.length} person(s) by the name ${req_param}: `);
-    console.log(`- 1: ${result.rows[0].first_name} ${result.rows[0].last_name}, born ${result.rows[0].birthdate}`);
+    console.log(`- ${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born ${moment(result.rows[0].birthdate).format('YYYY-MM-DD')}`);
     client.end();
   });
 });
